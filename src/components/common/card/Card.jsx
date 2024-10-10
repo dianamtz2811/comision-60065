2157971d0342742a2ee07f7e50abb26d82ac9394
[Diana Martínez] <[petitedifer@gmail.com]>
2024-10-10 12:07:37 -0600

    primer uso de mui

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -1,13 +1,23 @@
 import "./card.css"
+import Grid from '@mui/material/Grid2';
 
 export const Card = ({ title, price, stock }) => {
 
 
     return (
+        
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <div className="container-card">
-            <h2>Título: {title}</h2>
-            <h3>Precio: {price}</h3>
-            <h3>Stock: {stock}</h3>
+            <Grid size={6}>
+                <h2>Título: {title}</h2>
+            </Grid>
+            <Grid size={6}>
+                <h3>Precio: {price}</h3>
+            </Grid>
+            <Grid size={6}>
+                <h3>Stock: {stock}</h3>
+            </Grid>
         </div>
+        </Grid>
     );
 };
--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import { CartWidget } from "../../common/cartWidget/CartWidget"
 import "./navbar.css"
+import Grid from '@mui/material/Grid2';
 
 export const Navbar = () => {
     return (
-        <>
+    <>
+    <Grid>
         <div className="container-navbar">
             <img id="image" src="https://res.cloudinary.com/dg3bhw03n/image/upload/v1728361283/carrete_xbf5ra.png" alt="Carrete de Fotografía"/>
-            <h2>absorta</h2>
+            <Grid size={{xs:6}}>
+                <h2>absorta</h2>
+            </Grid>
 
             <ul>
                 <li>Todo</li>
@@ -19,6 +23,7 @@ export const Navbar = () => {
             <CartWidget />
 
         </div>
-        </>
+    </Grid>
+    </>
     )
 };

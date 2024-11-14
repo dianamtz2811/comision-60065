a4e484dcfa62b36efefc278311a0e5c6044b40ff
[Diana Martínez] <[petitedifer@gmail.com]>
2024-11-13 20:10:20 -0600

    Firebase II

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,6 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { CartContext } from "../../context/CartContext";
+import { db } from "../../../firebaseConfig";
+import { addDoc , collection , updateDoc , doc } from "firebase/firestore";
 
 const Checkout = () => {
+
+    const { cart, getTotalAmount, resetCart } = useContext( CartContext )
+
+    const [ orderId, setorderId] = useState(null);
+
     const [ userInfo, setUserInfo ] = useState({
         name:"",
         email:"",
@@ -10,13 +18,35 @@ const Checkout = () => {
     const funcionDelFormulario = (evento) => {
         evento.preventDefault();
         console.log(userInfo);
+        const total = getTotalAmount();
+
+    const order = {
+        buyer: userInfo,
+        items: cart,
+        total,
     };
+    let refCollection = collection( db, "orders" )
+    addDoc( refCollection, order ).then((res) => {
+        setorderId(res.id)
+        resetCart();
+    });
+
+    let refCol = collection(db, "products");
+    order.items.forEach((item) => {
+        let refDoc = doc(refCol, item.id);
+        updateDoc(refDoc, {stock: item.stock - item.quantity});
+    });
+};
+
 
     const capturarInfo = (evento) => {
         const { name, value } = evento.target;
         setUserInfo({...userInfo, [name]:value });
     }
 
+    if ( orderId ) {
+        return <h2>Gracias por tu compra, tu ticket es:{orderId}</h2>
+    }
     return (
     <div>
         <form onSubmit={funcionDelFormulario}>
@@ -40,6 +70,8 @@ const Checkout = () => {
         </form>
     </div>
     )
-}
+};
+
+
 
 export default Checkout
